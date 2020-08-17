1977cfae17280511e075e6b50f6a8892c2479073
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-08-17 11:16:01 -0600

    Corrección día 21 - Calculo de interes compuesto

--- challenge/day21.js
+++ challenge/day21.js
@@ -1,10 +1,7 @@
 const TASA_INTERES = 4; // %
 
 const calcularAhorro = (importe, meses) => {
-  let ahorro = importe;
-  for(let i = 1; i <= meses; i++) {
-    ahorro = ahorro + (ahorro * (TASA_INTERES/100))
-  }
+  const ahorro = importe * Math.pow((1 + TASA_INTERES/100), meses);
   return Math.round(ahorro * 100) / 100;
 }
 
@@ -13,7 +10,7 @@ const calculadora = () => {
   const importe = prompt(`${titulo} \n Importe inicial del ahorro.`);
   const meses = prompt(`${titulo} \n Meses de inversión en el ahorro.`);
 
-  const ahorro = calcularAhorro(parseFloat(importe), parseInt(meses));
+  const ahorro = calcularAhorro(parseFloat(importe), (parseInt(meses)/12));
 
   alert(`Ahorro total a ${meses} meses con un saldo inicial de $${importe} y una tasa de interes del ${TASA_INTERES}% es de $${ahorro}`);
 }
